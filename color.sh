fd7a6507ce8808ae3313bb6ac0ca0b9cb2063501
ambika-gunjetty <agunjetty@gmail.com>
2022-03-12 12:55:27 +0530

    simple script to print color

--- color.sh
+++ color.sh
@@ -1,3 +1,21 @@
 #/bin /bash
 
 echo Hello world
+
+## color world
+
+#Red      31
+#Green    32
+#Yellow   33
+#Blue     34
+#Magenta  35
+#Cyan     36
+
+# syntax : echo "\e[COLmHello\e[0m"
+# -e option is to enable esc seq, without -e colors will not work
+# " "Quotes are mandatory for colors to work, otherwise it will not work,optional we can use single quotes also preferred  way is  Doublequotes
+# \e[COLn->this to disable the color, COL is one of the code
+#\e[0m->this is to disable the color
+
+echo -e "\e[31meText in Red color"
+echo "one mor line"
