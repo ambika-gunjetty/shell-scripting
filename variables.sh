560ed67975f10147cf8f43ead9fa14f238143716
ambika-gunjetty <agunjetty@gmail.com>
2022-03-13 12:45:41 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+##!/bin/bash
+#
+#a=10
+#b=20
+#
+#echo $a
+#echo $b
+#
+#date=10-3-2027
+#echo today date is $date
 
-a=10
-b=20
-
-echo $a
-echo $b
-
-date=10-3-2027
-echo today date is $date
+x=10
+y=20
+ADD=$(($x+$y))
+echo add=$ADD
 
 
